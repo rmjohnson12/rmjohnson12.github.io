0e64938c675957de90b5c40bd84a6d4ee44112fc
rmjohnson12 <rmjohnson12@my.waketech.edu>
2025-04-11 09:19:13 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+    console.log("fetchGradeData() called - this will eventually fetch data from PostgreSQL.");
+    // Placeholder for future database interaction
+}
+
+function populateGradebook() {
+    console.log("populateGradebook() called - this will eventually insert rows into the table.");
+    // Placeholder for future logic to populate table
+}
+
+// For testing purposes, call both functions when the script loads
+fetchGradeData();
+populateGradebook();
